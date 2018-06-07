0c6a37cfae6cd7330dd9b9f465c935a5038ec660
Vizakenjack <vizakenjack@gmail.com>
2018-06-08 00:11:09 +0300

    Release 2.0.0

--- lib/bittrex.rb
+++ lib/bittrex.rb
@@ -1,94 +1,107 @@
-require "bittrex/version"
+require 'bittrex/version'
 require 'openssl'
 require 'json'
-require 'open-uri'
+require 'httparty'
 
 module Bittrex
-  class << self
-    API_VERSION = 'v1.1'
+  class Request
+      BASE_URL = 'https://bittrex.com/api/v1.1'
+      attr_reader :api_key, :api_secret
 
-    def new(api_key, api_secret, trade_for = "BTC")
-      @api_key = api_key
-      @api_secret = api_secret
-      @trade_for = trade_for + "-"
-      self
+      def initialize(api_key, api_secret)
+        @api_key = api_key
+        @api_secret = api_secret
+      end
+
+      def get(path, params = '')
+        begin
+          nonce = Time.now.to_i
+          final_url = "#{BASE_URL}#{path}?apikey=#{api_key}&nonce=#{nonce}&"+params
+          hmac_sign = generate_sign(final_url)
+
+          response = HTTParty.get(final_url, headers: { apisign: hmac_sign })
+          handle_response response.body
+        rescue Exception => e
+          nil
+        end
+      end
+
+      private
+
+      def generate_sign(url)
+        OpenSSL::HMAC.hexdigest('sha512', api_secret, url)
+      end
+
+      def handle_response(response)
+        json = JSON.parse(response)
+        if json['success']
+          json['result']
+        else
+          puts "Request failed: #{json}"
+          json['message']
+        end
+      end
+  end
+
+  class Api
+    attr_accessor :request
+
+    def initialize(api_key, api_secret)
+      @request = Request.new(api_key, api_secret)
     end
 
     def ticker(market)
-      request("https://bittrex.com/api/#{API_VERSION}/public/getticker", "market=#{@trade_for}#{market}")
+      request.get "/public/getticker", "market=#{market}"
     end
 
     def summaries
-      request("https://bittrex.com/api/#{API_VERSION}/public/getmarketsummaries")
+      request.get "/public/getmarketsummaries"
     end
 
     def orderbook(market, type, depth = 50)
-      request("https://bittrex.com/api/#{API_VERSION}/public/getorderbook", "market=#{@trade_for}#{market}&type=#{type}&depth=#{depth}")
+      request.get "/public/getorderbook", "market=#{market}&type=#{type}&depth=#{depth}"
     end 
 
     def market_history(market, count = 10)
-      request("https://bittrex.com/api/#{API_VERSION}/public/getmarkethistory", "market=#{@trade_for}#{market}&count=#{count}")
+      request.get "/public/getmarkethistory", "market=#{market}&count=#{count}"
     end
-
 
     def buy(market, quantity, rate = nil)
       if rate
-        request("https://bittrex.com/api/#{API_VERSION}/market/buylimit", "market=#{@trade_for}#{market}&quantity=#{quantity}&rate=#{rate}")
+        request.get "/market/buylimit", "market=#{market}&quantity=#{quantity}&rate=#{rate}"
       else
-        request("https://bittrex.com/api/#{API_VERSION}/market/buymarket", "market=#{@trade_for}#{market}&quantity=#{quantity}")
+        request.get "/market/buymarket", "market=#{market}&quantity=#{quantity}"
       end
     end
 
     def sell(market, quantity, rate = nil)
       if rate
-        request("https://bittrex.com/api/#{API_VERSION}/market/selllimit", "market=#{@trade_for}#{market}&quantity=#{quantity}&rate=#{rate}")
+        request.get "/market/selllimit", "market=#{market}&quantity=#{quantity}&rate=#{rate}"
       else
-        request("https://bittrex.com/api/#{API_VERSION}/market/sellmarket", "market=#{@trade_for}#{market}&quantity=#{quantity}")
+        request.get "/market/sellmarket", "market=#{market}&quantity=#{quantity}"
       end
     end
 
     def cancel(order_id)
-      request("https://bittrex.com/api/#{API_VERSION}/market/cancel", "uuid=#{order_id}")
+      request.get "/market/cancel", "uuid=#{order_id}"
     end
 
     def open_orders(market = '')
-      request("https://bittrex.com/api/#{API_VERSION}/market/getopenorders", "market=#{@trade_for}#{market}")
+      request.get "/market/getopenorders", "market=#{market}"
+    end
+
+    def balances
+      request.get "/account/getbalances"
     end
 
     def balance(currency)
-      request("https://bittrex.com/api/#{API_VERSION}/account/getbalance", "currency=#{currency}")
+      request.get "/account/getbalance", "currency=#{currency}"
     end
 
     def order_history(market = nil, count = 5)
-      params = market ? "market=#{@trade_for}#{market}&count=#{count}" : "count=#{count}"
-      request("https://bittrex.com/api/#{API_VERSION}/account/getorderhistory", params)
-    end
-
-    private
-
-    def generate_sign(url, params)
-      nonce = Time.now.to_i
-      @final_url = "#{url}?apikey=#{@api_key}&nonce=#{nonce}&"+params
-      OpenSSL::HMAC.hexdigest(digest = OpenSSL::Digest.new('sha512'), @api_secret, @final_url)
-    end
-
-    def handle_response(req)
-      response = JSON.load(req)
-      if response['success']
-        response['result']
-      else
-        puts "Request failed: #{response}"
-        response['message']
-      end
-    end
-
-    def request(url, params = '')
-      begin
-        hmac_sign = generate_sign(url, params)
-        handle_response open(@final_url, 'apisign' => hmac_sign)
-      rescue
-        return false
-      end
+      params = market ? "market=#{market}&count=#{count}" : "count=#{count}"
+      request.get "/account/getorderhistory", params
     end
   end
+
 end
--- lib/bittrex/version.rb
+++ lib/bittrex/version.rb
@@ -1,3 +1,3 @@
 module Bittrex
-  VERSION = "1.0.0"
+  VERSION = "2.0.0"
 end
